777ad8bace208c546fa483761406bcfcb0cf27f6
Edvar <fodose123@gmail.com>
2023-07-20 20:48:20 -0300

    :sparkle: fix: images 404

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,56 +1,71 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export default function Movies() {
-    const [data, setData] = useState({});
-    const [title, setTitle] = useState({});
+  const [data, setData] = useState({});
+  const [title, setTitle] = useState('');
 
-
-    useEffect(() => {
-        async function getData() {
-            const received = await fetch(`https://api.themoviedb.org/3/search/movie?query=${title}`, {
-                method: "GET",
-                headers: {
-                    accept: "application/json",
-                    Authorization:
-                        `Bearer ${process.env.REACT_APP_TOKEN}`,
-                },
-            });
-    
-            const response = await received.json();
-            console.log(response);
-            setData(response);
+  useEffect(() => {
+    async function getData() {
+      const received = await fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${title}`,
+        {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+          },
         }
+      );
 
-        getData()
+      const response = await received.json();
+      console.log(response);
+      setData(response);
+    }
 
-    }, [title])
+    getData();
+  }, [title]);
 
-
-    return(
-        <div>
-            <button onClick={() => {
-                const body = document.querySelector("body");
-                body.classList.toggle("dark");
-                const button = document.querySelector("button");
-                button.textContent = body.classList.contains("dark") ? "Light Mode" : "Dark Mode";
-            }}>
-                Dark Mode
-            </button>
-            <h1>Busca de filmes?</h1>
-            <form onSubmit={(e) => e.preventDefault()}>
-                <input type="text" onChange={(e) => setTitle(e.target.value)} />
-                <button>Pesquisar</button>
-            </form>
-            <div className="movie-container">
-                {data.results?.map((item) => (
-                    <div key={item.id}>
-                        <h4 style={{ textAlign: "center", maxWidth: "300px" }}>
-                            {item.title} ({new Date(item.release_date).getFullYear()})
-                        </h4>
-                        <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title} width="300px" />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        onClick={() => {
+          const body = document.querySelector('body');
+          body.classList.toggle('dark');
+          const button = document.querySelector('button');
+          button.textContent = body.classList.contains('dark')
+            ? 'Light Mode'
+            : 'Dark Mode';
+        }}
+      >
+        Dark Mode
+      </button>
+      <h1>Qual o filme?</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button>Pesquisar</button>
+      </form>
+      <div className="movie-container">
+        {data.results?.map((item) => (
+          <div key={item.id}>
+            {item.poster_path && (
+              <>
+                <h4 style={{ textAlign: 'center', maxWidth: '300px' }}>
+                  {item.title} ({new Date(item.release_date).getFullYear()})
+                </h4>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                  alt={item.title}
+                  width="300px"
+                  height="450px"
+                />
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
