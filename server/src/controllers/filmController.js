5ea418e063766f70ad26bf14a6e3d6cd8d6b5273
Anthony D <anthonydantas15@gmail.com>
2023-08-14 16:28:09 -0400

    :sparkles: feat: router getFilm by api tmdb

--- server/src/controllers/filmController.js
+++ server/src/controllers/filmController.js
@@ -1,13 +1,13 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const express = require('express');
+const express = require("express");
 
 async function getFilms(request, response) {
   try {
     const films = await prisma.filme.findMany();
     response.status(200).send(films);
   } catch (err) {
-    response.status(500).send({ error: 'Internal server error' });
+    response.status(500).send({ error: "Internal server error" });
   }
 }
 
@@ -37,11 +37,35 @@ async function createFilm(request, response) {
     });
     response.status(201).send(newFilm);
   } catch (err) {
-    response.status(500).send({ error: 'Internal server error' });
+    response.status(500).send({ error: "Internal server error" });
+  }
+}
+
+async function getFilme(request, response) {
+  try {
+    const { title } = request.params;
+
+    const received = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${title}`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${process.env.TOKEN_TMDB_API}`,
+        },
+      }
+    );
+
+    const filmData = await received.json();
+
+    return response.status(201).send(filmData.results);
+  } catch (err) {
+    response.status(500).send(err);
   }
 }
 
 module.exports = {
   getFilms,
   createFilm,
+  getFilme,
 };
--- server/src/routes/filmRoute.js
+++ server/src/routes/filmRoute.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const filmController = require('../controllers/filmController');
+const express = require("express");
+const filmController = require("../controllers/filmController");
 
 const router = express.Router();
 
-router.get('/', filmController.getFilms);
-router.post('/', filmController.createFilm);
+router.get("/", filmController.getFilms);
+router.post("/", filmController.createFilm);
+router.get("/:title", filmController.getFilme);
 
 module.exports = router;
