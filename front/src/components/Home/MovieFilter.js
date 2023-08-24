de3db7a0063b9b79b9ab4bacfe28935b28fe603c
João Lucas <jloc@ic.ufal.br>
2023-08-24 13:47:09 -0300

    feat: adding temporary change in `FilterButton`

--- front/src/components/Home/MovieFilter.js
+++ front/src/components/Home/MovieFilter.js
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import FilterButton from "./FilterButton";
-import { FaClock, FaEye, FaCircleCheck, FaCircleXmark } from "react-icons/fa6";
+import { FaClock, FaCircleCheck, FaCircleXmark } from "react-icons/fa6";
 
 export default function MovieFilter() {
+  const [filter, setFilter] = useState("completed");
+
   return (
     <div className="flex bg-primary-700 rounded-xl text-lg flex-1 gap-2 px-2">
-      <FilterButton hover="hover:bg-[#BBB755]" color="bg-[#BBB755]">
+      <FilterButton
+        hover="hover:bg-secondary-700"
+        color="bg-secondary-700"
+        active={filter === "plan"}
+        onClick={() => setFilter("plan")}
+      >
         <FaClock size={24} className="mr-3 mt-0.5" />
         Plan to watch
       </FilterButton>
-      <FilterButton hover="hover:bg-secondary-700" color="bg-secondary-700" active>
-        <FaEye size={24} className="mr-3 mt-0.5" />
-        Watching
-      </FilterButton>
-      <FilterButton hover="hover:bg-[#609F6E]" color="bg-[#609F6E]">
+      <FilterButton
+        hover="hover:bg-[#609F6E]"
+        color="bg-[#609F6E]"
+        active={filter === "completed"}
+        onClick={() => {
+          setFilter("completed");
+        }}
+      >
         <FaCircleCheck size={24} className="mr-3 mt-0.5" />
         Completed
       </FilterButton>
-      <FilterButton hover="hover:bg-[#9F6060]" color="bg-[#9F6060]">
+      <FilterButton
+        hover="hover:bg-[#9F6060]"
+        color="bg-[#9F6060]"
+        active={filter === "dropped"}
+        onClick={() => setFilter("dropped")}
+      >
         <FaCircleXmark size={24} className="mr-3 mt-0.5" />
         Dropped
       </FilterButton>
