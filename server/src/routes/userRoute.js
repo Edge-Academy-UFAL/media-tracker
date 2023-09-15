1667a532c0613d14369b071d2160d08b336f6787
João Lucas <jloc@ic.ufal.br>
2023-09-14 21:39:28 -0300

    🛠️ fix: fixing typo in routes

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -8,7 +8,7 @@ router.get("/", loginRequired, userController.getUser);
 router.post("/", userController.createUser);
 router.post("/login", userController.loginUser);
 router.get("/movies", loginRequired, userController.getMovies);
-router.post("/movies/:tmbdId", loginRequired, userController.addMovie);
-router.put("/movies/:tmbdId", loginRequired, userController.updateStatus);
+router.post("/movies/:tmdbId", loginRequired, userController.addMovie);
+router.put("/movies/:tmdbId", loginRequired, userController.updateStatus);
 
 module.exports = router;
