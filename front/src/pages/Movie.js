ae83798dfafe23eeb4202a7eeceb702ed9cc03f5
João Lucas <jloc@ic.ufal.br>
2024-01-23 15:42:15 -0300

    ⚙️ fix: fixing REACT_APP_AI_API_URL

--- front/src/pages/Movie.js
+++ front/src/pages/Movie.js
@@ -154,7 +154,7 @@ export default function Movie() {
       const cast = credits.cast.map((person) => person.name);
       const crew = credits.crew.filter((person) => person.job === "Director").map((person) => person.name);
 
-      const response = await fetch(${process.env.REACT_APP_AI_API_URL}/recommend/`, {
+      const response = await fetch(`${process.env.REACT_APP_AI_API_URL}/recommend/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
